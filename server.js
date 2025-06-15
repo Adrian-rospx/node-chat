c49687f799be2e0b8f53edf260682d1a4e068271
Adrian-rospx <adrianrosu5400@gmail.com>
2025-06-15 22:18:27 +0300

    update: simple http client server

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ import http from "node:http";
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-
-    res.writeHead(200, {'content-type' : 'text/html'});
-
-    res.end("<h1>Hello, node.js!</h1>");
+    // basic client server setup
+    if(req.url === '/') {
+        res.writeHead(200, {'content-type' : 'text/html'});
+        res.end("<h1> Welcome to the Homepage </h1>");
+    } else if(req.url === '/about') {
+        res.writeHead(200, {'content-type' : 'text/html'});
+        res.end("<h1>This page was written by Adrian</h1>");
+    } else {
+        res.writeHead(404, {'content-type' : 'text/html'});
+        res.end("<h1>This address is not available</h1>");
+    }
 })
 
 server.listen(PORT, () => {
